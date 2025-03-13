c7787a8817ca05ad8648ecb351830fd8c694bd08
Prikshit Sharma <prikshitcodes@gmail.com>
2025-03-13 17:21:48 +0530

    Add files via upload

---
+++ 06_memory.js
@@ -0,0 +1,21 @@
+//Stack (Primtive) and Heap(Non-Primitve)
+
+let myYoutubeName ="PrikshitSharmadotcom"
+
+let anotherName = myYoutubeName
+
+console.log(anotherName);
+anotherName="babakadaba"
+console.log(anotherName);
+
+
+let  userOne ={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+userTwo.upi="user2@ybl"
+
+
+console.log(userOne);
